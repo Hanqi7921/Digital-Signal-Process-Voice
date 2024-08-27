600ba0cfeecbd8fcefaa9ce0fe01a10a78c2906d
Hanqi <lvhanqi@shu.edu.cn>
2024-08-27 23:55:04 +0800

    测试读取、录制语音成功

---
+++ audio_record.py
@@ -0,0 +1,10 @@
+import pyAudioKits.record
+import pyAudioKits.audio as ak
+audio = ak.read_Audio(direction = "E:\Projects\Class\DSP_project\gui\conv.wav")
+audio.sound()
+# sr=1000
+# recordSeconds=5
+# audio = pyAudioKits.record.record(sr,recordSeconds)
+# audio.sound()
+
+# audio = ak.Audio(samples, sr)
