7adebd12a0c0ce4e087395ed5ff4f3e28b9d945e
Hanqi <lvhanqi@shu.edu.cn>
2024-08-28 19:50:27 +0800

    改版界面为四幅图，语音端点检测初步成功，效果待提升

---
+++ Speech_Endpoint.py
@@ -0,0 +1,17 @@
+import pyAudioKits.audio as ak
+import numpy as np
+import pyAudioKits.analyse as aly
+import pyAudioKits.algorithm as alg
+from png2gif import PNG2GIF
+
+#https://github.com/HarmoniaLeo/pyAudioKits/blob/main/6.%20Endpoint%20Detection%20and%20Speech%20Recognition.ipynb
+def Speech_Endpoint(detect_audio):
+    vad_result = alg.VAD(detect_audio, 0.05, 0.5, 400)    
+    # '''
+    # Endpoint detection for the recording, 
+    # setting a lower short-time energy threshold of 0.05, 
+    # a higher short-time energy threshold of 0.5, 
+    # and a short-time over-zero rate threshold of 400
+    # '''
+    vad_result.plot(imgPath="Img_result\PNG\change.png")
+    PNG2GIF()
--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from tkinter import PhotoImage, Button, Label, messagebox
 from audio_record import Audio
 from tkinter import filedialog
 from Speech_Enhancement import Spectral_subtraction
+from Speech_Endpoint import Speech_Endpoint
 
+new_image_path_1 = "Img_result\PNG\original.png"
+new_image_path_2 = "Img_result\PNG\change.png"
 class Application(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
@@ -25,21 +28,35 @@ class Application(tk.Frame):
         self.left_frame.grid(row=1, column=0, padx=10, pady=10, sticky="ns")
 
         # Load and display images
-        self.photo1 = PhotoImage(file="Img_result\GIF\original.gif")
-        # 设置图片的最大宽度和高度
-        max_width = 420  # 例如，最大宽度为200像素
-        max_height = 350  # 例如，最大高度为200像素
+        self.photo1 = PhotoImage(file="Img_result/GIF/original.gif")
+        self.photo2 = PhotoImage(file="Img_result/GIF/change.gif")
+        self.photo3 = PhotoImage(file="Img_result/GIF/original.gif")
+        self.photo4 = PhotoImage(file="Img_result/GIF/original.gif")
+        
+        # Resize images if needed
+        max_width = 450
+        max_height = 250
         self.photo1 = self.photo1.subsample(max(self.photo1.width() // max_width, 1),
                                             max(self.photo1.height() // max_height, 1))
-        self.original_img = Label(self.left_frame, image=self.photo1)
-        self.original_img.grid(row=0, column=0)
-
-        self.photo2 = PhotoImage(file="Img_result\GIF\original.gif")
-        # 同样设置第二张图片的大小
         self.photo2 = self.photo2.subsample(max(self.photo2.width() // max_width, 1),
                                             max(self.photo2.height() // max_height, 1))
-        self.change_img = Label(self.left_frame, image=self.photo2)
-        self.change_img.grid(row=0, column=1, padx=(10, 0))  # Add some padding between
+        self.photo3 = self.photo3.subsample(max(self.photo3.width() // max_width, 1),
+                                            max(self.photo3.height() // max_height, 1))
+        self.photo4 = self.photo4.subsample(max(self.photo4.width() // max_width, 1),
+                                            max(self.photo4.height() // max_height, 1))
+        
+        # Create image labels
+        self.img1 = Label(self.left_frame, image=self.photo1)
+        self.img1.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+        
+        self.img2 = Label(self.left_frame, image=self.photo2)
+        self.img2.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
+        
+        self.img3 = Label(self.left_frame, image=self.photo3)
+        self.img3.grid(row=1, column=0, padx=5, pady=5, sticky="nsew")
+        
+        self.img4 = Label(self.left_frame, image=self.photo4)
+        self.img4.grid(row=1, column=1, padx=5, pady=5, sticky="nsew")
 
         # Create a right frame for buttons
         self.right_frame = tk.Frame(self)
@@ -61,7 +78,7 @@ class Application(tk.Frame):
         self.btn_delEcho = Button(self.right_frame, text="去除回声", width=12, height=2, command=self.Callback_DelEcho)
         self.btn_delEcho.grid(row=4, column=0, pady=5)
 
-        self.btn_endRecog = Button(self.right_frame, text="端点检测", width=12, height=2, command=self.Callback_Recognition)
+        self.btn_endRecog = Button(self.right_frame, text="端点检测", width=12, height=2, command=self.Callback_Detection)
         self.btn_endRecog.grid(row=5, column=0, pady=5)
         
         self.btn_play = Button(self.right_frame, text="播放音频", width=12, height=2, command=self.Callback_Play)
@@ -108,8 +125,9 @@ class Application(tk.Frame):
         messagebox.showinfo("提示", "去除回声")
         print("去除回声")
 
-    def Callback_Recognition(self):
-        messagebox.showinfo("提示", "端点检测")
+    def Callback_Detection(self):
+        Speech_Endpoint(audio.object)
+        self.update_image(self.img2, new_image_path_2)  # 更新第二张图片
         print("端点检测")
 
     def Callback_Play(self):
@@ -117,15 +135,14 @@ class Application(tk.Frame):
         audio.play(audio.object)
         audio.plot(audio.object)
         print("播放音频")
-        new_image_path = "Img_result\PNG\original.png"
-        self.update_image(self.original_img, new_image_path)  # 更新第一张图片
-        self.update_image(self.change_img, new_image_path)  # 更新第二张图片
+        self.update_image(self.img1, new_image_path_1)  # 更新第一张图片
+
     
     def update_image(self, label, image_path):
         # 加载新图片并调整大小
         new_photo = PhotoImage(file=image_path)
-        max_width = 420
-        max_height = 350
+        max_width = 450
+        max_height = 250
         new_photo = new_photo.subsample(max(new_photo.width() // max_width, 1),
                                         max(new_photo.height() // max_height, 1))
         # 更新Label的图像
@@ -137,7 +154,7 @@ class Application(tk.Frame):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("数字信号处理")
-    root.geometry("1200x500+100+100")
+    root.geometry("1200x700+100+100")
     app = Application(root)
     audio=Audio()
     root.mainloop()
